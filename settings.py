5a0212512ce6071059a4de422490a28f183e4b65
d7ome-debug <127843856+d7ome-debug@users.noreply.github.com>
2023-05-24 21:31:23 +0300

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,17 @@
+# Postgre SQL
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'd7ome121121',
+        'USER': 'd7ome121121',
+        'PASSWORD': 'MTM1MzctZDdvbWUx',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
+
+INSTALLED_APPS = (
+    'orm',
+)
+
+SECRET_KEY = 'SECRET KEY for this Django Project'
